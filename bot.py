574bb00ba761c060a1992112dd343121b8ba299f
asus <naimov.bunyod2021@gmail.com>
2025-05-08 09:37:18 +0500

    added session bot

--- bot.py
+++ bot.py
@@ -5,9 +5,14 @@ from aiogram import Bot, Dispatcher
 from commands import router as commands_router
 from handlears import router as handlears_router
 from dotenv import load_dotenv
+from aiogram.client.session.aiohttp import AiohttpSession
 load_dotenv()
 
+
+
 TOKEN = getenv("BOT_TOKEN")
+
+session = AiohttpSession(proxy="http://proxy.server:3128")
 
 dp = Dispatcher()
 
@@ -17,7 +22,8 @@ dp.include_router(handlears_router)
 
 
 async def main() -> None:
-    bot = Bot(token=TOKEN)
+    # bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, session=session)
     await dp.start_polling(bot)
 
 
