0e13f6d9156df21d9fdd26149370d569b0176ad1
asus <naimov.bunyod2021@gmail.com>
2025-05-17 10:04:35 +0500

    fixed

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ tg.MainButton.color = "#af0808";
 
 btn1.addEventListener("click", function (){
     if (tg.MainButton.isVisible()){
-        tg.MainButton.hide()
+        tg.MainButton.hide();
     }
     else {
-        tg.MainButton.show()
+        tg.MainButton.show();
     }
-})
+});
 btn2.addEventListener("click", function (){
     tg.MainButton.show()
-})
+});
 btn3.addEventListener("click", function (){
     tg.MainButton.show()
-})
+});
