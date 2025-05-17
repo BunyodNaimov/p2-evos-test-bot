4e7b52523adbb1d575e38a374627dda79ed98e40
asus <naimov.bunyod2021@gmail.com>
2025-05-17 09:56:07 +0500

    added app.js file

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#af0808";
 
 btn1.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide()
+    }
+    else {
+        tg.MainButton.show()
+    }
+})
+btn2.addEventListener("click", function (){
+    tg.MainButton.show()
+})
+btn3.addEventListener("click", function (){
     tg.MainButton.show()
 })
